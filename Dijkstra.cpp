ae6305124b7c9f1c39111c956751da1fa4cf6c28
nreyog <75873044+nreyog@users.noreply.github.com>
2022-04-15 17:21:01 +0800

    获取从start到其他点的最短路径长度

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 struct Dijkstra {
+
     vector<int> dist;
     vector<vector<pair<int, int>>> g;
 
-    void init(int n) {  // 初始化
+    Dijkstra(int n) {   // 构造函数
         dist.resize(n + 5, 1e9);
         g.resize(n + 5);
     }
@@ -16,7 +17,7 @@ struct Dijkstra {
         g[from].push_back({to, value});
     }
 
-    void get_dist(int start) {  // 获取从start到其他点的最短路径
+    void get_dist(int start) {  // 获取从start到其他点的最短路径长度
         dist[start] = 0;
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
         q.push({0, start});
@@ -37,8 +38,7 @@ struct Dijkstra {
 int main() {
     int n, m, s, t;
     cin >> n >> m >> s >> t;
-    Dijkstra D;
-    D.init(n);
+    Dijkstra D(n);
     while (m--) {
         int u, v, w;
         cin >> u >> v >> w;
